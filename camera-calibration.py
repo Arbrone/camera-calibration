c77e01ac76bd2bad1bcbc2f62f12184581e53b17
Arbrone <thomas.verrier@hotmail.com>
2023-11-10 13:42:06 +0100

    gestreamer pipeline

--- camera-calibration.py
+++ camera-calibration.py
@@ -5,12 +5,32 @@ import numpy as np
 import glob
 import pickle
 
+def gstreamer_pipeline(capture_width=1280,capture_height=720,display_width=640,display_height=360,framerate=60,flip_method=0):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 def capture_images(device, name):
     save = f'images/{name}'
     if not os._exists(save):
         os.makedirs(save,exist_ok=True)
 
-    cap = cv2.VideoCapture(device)
+    cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
     cpt = 0
     
     while(True): 
